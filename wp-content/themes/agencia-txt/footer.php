88a3903e2c514d434c8a26837e5db6e3b4cb7402
digital <digital@txt.co>
2018-11-15 16:29:49 -0500

    se configuro el menu y  parte de footer con los archivos bootstrap

--- wp-content/themes/agencia-txt/footer.php
+++ wp-content/themes/agencia-txt/footer.php
@@ -13,27 +13,57 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'href="http://txt.co', 'agencia-txt' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Producto creado por: %s', 'agencia-txt' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'agencia-txt' ), 'agencia-txt', '<a href="http://txt.co">Txt Agencia Transmedia</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+		<div class="container">
+			<div class="row">
+					<footer id="colophon" class="site-footer">
+				<div class="site-info">
+					<a href="<?php echo esc_url( __( 'href="http://txt.co', 'agencia-txt' ) ); ?>">
+						<?php
+						/* translators: %s: CMS name, i.e. WordPress. */
+						printf( esc_html__( 'Producto creado por: %s', 'agencia-txt' ), 'WordPress' );
+						?>
+					</a>
+					<span class="sep"> | </span>
+						<?php
+						/* translators: 1: Theme name, 2: Theme author. */
+						printf( esc_html__( 'Theme: %1$s by %2$s.', 'agencia-txt' ), 'agencia-txt', '<a href="http://txt.co">Txt Agencia Transmedia</a>' );
+						?>
+				</div><!-- .site-info -->
+			</footer><!-- #colophon -->
+
+			</div>
+		</div>
+	</div><!-- #page -->
+
+	<footer class="page-footer ">
+	  <div class="container">
+	    <div class="row">
+ 			 		<div class="col-12 mt-2 mt-sm-5">
+ 			 			<ul class="list-unstyled list-inline social text-center">
+ 			 				<li class="list-inline-item"><a href=""><i class="fab fa-facebook-square"></i></a></li>
+							<li class="list-inline-item"><a href="" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+	 						<li class="list-inline-item"><a href=""><i class="fab fa-instagram"></i></a></li>
+ 			    	  <li class="list-inline-item"><a href=""><i class="fab fa-google-plus-square"></i></a></li>
+					<li class="list-inline-item"><a href=""><i class="fas fa-phone"></i></a></li>
+				</ul>
+ 			</div>
+	    </div>
+		<div class="row">
+		  <div class="col-12  mb-sm-5">
+			<h5 class="text-center">Dirección: carrera 12 No. 87-45 / Teléfono: 691 8788 <br>
+					Bogotá, Colombia<br>
+		      <a href="">2018 © Txt Agencia Transmedia</a>
+		    </h5>
+		  </div>
+		</div>
+	  </div>
+ 	</footer>
 
 <?php wp_footer(); ?>
-<script src="<?php bloginfo( 'template_url' ); ?>/js/jquery-3.3.1.slim.min.js"></script>
-<script src="<?php bloginfo( 'template_url' ); ?>/js/popper.min.js"></script>
+
 </div>
 </div>
+
+
 </body>
 </html>
--- wp-content/themes/agencia-txt/header.php
+++ wp-content/themes/agencia-txt/header.php
@@ -17,45 +17,41 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link rel="stylesheet" href="<?php bloginfo( 'template_url' ); ?>/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
+	<script src="<?php bloginfo( 'template_url' ); ?>/js/jquery-3.3.1.slim.min.js"></script>
+	<script src="<?php bloginfo( 'template_url' ); ?>/js/popper.min.js"></script>
 	<script src="<?php bloginfo( 'template_url' ); ?>/js/bootstrap.min.js"></script>
-	<?php wp_head(); ?>
+<?php wp_head(); ?>	
 </head>
 
 <body <?php body_class(); ?>>
-	<div class="container">
+
  
-<div id="page" class="site row">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'agencia-txt' ); ?></a>
-
+<div id="page" class="site">
+	<!-- <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'agencia-txt' ); ?></a>
+	 -->
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$agencia_txt_description = get_bloginfo( 'description', 'display' );
-			if ( $agencia_txt_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $agencia_txt_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'agencia-txt' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
+		<nav class="navbar navbar-light bg-light">
+			<div class="container">
+			    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+			    <span class="navbar-toggler-icon"></span>
+			    </button>
+		  		<a class="navbar-brand" href="#">Navbar</a>
+		  		<div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+		  			<?php
+					wp_nav_menu( 
+						array(
+						'container' => false,
+						'items_wrap' => '<ul id="menu-top" class="navbar-nav mr-auto mt-2 mt-lg-0">%3$s</ul>',
+						'theme_location' => 'menu-1',
+						'menu_id'        => 'primary-menu',
+					) );
+					?>
+		  		</div>
+			</div>
+		</nav>
 	</header><!-- #masthead -->
-
+	
+<div class="container">
+	<div class="row">
 	<div id="content" class="site-content">
