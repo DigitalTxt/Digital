20f8c05e69d411277d9cdccfe56b1cef87fe1479
AlejandroLezcano <alejandrolezcano24@gmail.com>
2018-11-16 18:01:54 -0500

    Pruebas pagina (Mito-Rito)

---
+++ wp-content/themes/base-bootstrap/prueba nuevo TXT/js/java-script.js
@@ -0,0 +1,15 @@
+
+function prueba(){
+
+
+  document.getElementById('cover-id').className += " cover1";
+
+
+}
+
+function atras(){
+
+document.getElementById('cover-id').className -= " cover1";
+
+
+}
